4c8b75f8006641996cd9f37d7cf748bd16eb0826
Alex Falkowski <alexrfalkowski@gmail.com>
2025-05-01 07:28:48 +0200

    test(articles): manually start the system to verify conditions

--- test/lib/sasha.rb
+++ test/lib/sasha.rb
@@ -12,6 +12,26 @@ require 'sasha/v1/server'
 module Sasha
   module V1
     class << self
+      def bucket_operational?
+        bucket_state == 'operational'
+      end
+
+      def bucket_missing?
+        bucket_state == 'missing'
+      end
+
+      def bucket_erroneous?
+        bucket_state == 'erroneous'
+      end
+
+      def apply_bucket_state(state)
+        ENV['BUCKET_STATE'] = state
+      end
+
+      def bucket_state
+        ENV.fetch('BUCKET_STATE', nil)
+      end
+
       def client
         @client ||= Sasha::V1::Client.new(Nonnative.configuration.url)
       end
